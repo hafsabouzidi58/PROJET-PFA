bae21ffc267d364088b5a142cb10a9579a3758c1
HAFSA <bouzidihafsa75@gmail.com>
2026-06-26 16:50:52 +0100

    Fix: correct base directory to projet-pfa and add netlify runtime

--- gestion-stock-saas/next.config.ts
+++ gestion-stock-saas/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // <-- Ajoute cette ligne si tu veux un export statique HTML (génèrera un dossier "out")
+  /* tes autres configurations si existantes */
 };
-
 export default nextConfig;
